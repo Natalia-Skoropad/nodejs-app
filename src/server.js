6c23136bb4f9149d5897957a9f49f4fbfc698fa7
Nata <skoropad_natalia@ukr.net>
2025-12-01 14:48:47 +0200

    Initial commit

---
+++ src/server.js
@@ -0,0 +1,39 @@
+import express from 'express';
+import 'dotenv/config';
+import cors from 'cors';
+
+import { logger } from './middleware/logger.js';
+import { notFoundHandler } from './middleware/notFoundHandler.js';
+import { errorHandler } from './middleware/errorHandler.js';
+
+import { connectMongoDB } from './db/connectMongoDB.js';
+import studentsRoutes from './routes/studentsRoutes.js';
+
+//=================================================================
+
+const app = express();
+const PORT = process.env.PORT ?? 3000;
+
+//=================================================================
+
+app.use(logger);
+app.use(express.json());
+app.use(cors());
+
+//=================================================================
+
+app.use(studentsRoutes);
+app.use(notFoundHandler);
+app.use(errorHandler);
+
+//=================================================================
+
+await connectMongoDB();
+
+//=================================================================
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+//=================================================================
